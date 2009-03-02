d1feea694a3bd06594faffa42677d7a8ae2cf8e6
dan.j.allen <dan.j.allen@gmail.com>
2009-03-02 05:12:14 +0000

    bump version number

--- src/PanoJS.js
+++ src/PanoJS.js
@@ -1,5 +1,5 @@
 /**
- * Panoramic JavaScript Image Viewer (PanoJS) 1.0
+ * Panoramic JavaScript Image Viewer (PanoJS) 1.0.2
  *
  * Generates a draggable and zoomable viewer for images that would
  * be otherwise too large for a browser window.  Examples would include
